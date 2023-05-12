dd0891425c5a8f69ae28b19c67cf469fbd3ab0b1
kayibal <alan@datarevenue.com>
2023-05-13 00:02:10 +0100

    Use webpack
    nucypher has some wasm parts, so we need to use webpack to even be able to import the correspondings modules from the package.
    
    Most of these configurations and webpack config are taken from their examples. I purposefully did not include any react components which they include in their examples.

--- src/privacy/threshold.ts
+++ src/privacy/threshold.ts
@@ -1,6 +1,14 @@
 export const threshold: string = "Threshold Order Privacy"
 
+import { Cohort } from '@nucypher/nucypher-ts';
+
+const config = {
+    threshold: 3,
+    shares: 5,
+    porterUri: 'https://porter-tapir.nucypher.community',
+};
 
 export function encrypt(): void {
+    const cohort = Cohort.create(config);
     console.log("Hello from the threshold privacy module!");
 }
