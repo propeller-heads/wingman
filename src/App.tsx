30a4c7bb574e95bf8b8bdfad2389e9148a742484
czanella <carlos@datarevenue.com>
2023-05-13 08:53:48 +0100

    WIP Add main UI elements and styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,25 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import MainForm from './components/stopLoss';
+import { Box } from '@chakra-ui/react';
+import styles from './App.module.css';
+
 
 function App() {
+  const handleFormSubmit = (data: any) => {
+    // Handle form submission here
+    console.log(data);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={`App ${styles.backgroundImage}`}>
+      <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center">
+        {/* <h1 className={styles.appName}>wingman</h1> */}
+        <MainForm onSubmit={handleFormSubmit} />
+      </Box>
     </div>
   );
 }
 
 export default App;
+
