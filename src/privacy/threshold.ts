ef8b35bbec29e5d4aae499543493715f205a49ae
kayibal <alan@datarevenue.com>
2023-05-13 08:03:39 +0100

    Adds a condition to check against chainlink price.

--- src/privacy/threshold.ts
+++ src/privacy/threshold.ts
@@ -1,14 +1,48 @@
 export const threshold: string = "Threshold Order Privacy"
 
-import { Cohort } from '@nucypher/nucypher-ts';
+import { Cohort, Conditions, Condition } from '@nucypher/nucypher-ts';
 
 const config = {
-    threshold: 3,
-    shares: 5,
+    threshold: 2,
+    shares: 3,
     porterUri: 'https://porter-tapir.nucypher.community',
 };
 
-export function encrypt(): void {
-    const cohort = Cohort.create(config);
+
+export function createPriceCondition(
+    oracleAddress: string,
+    triggerPrice: number, // TODO: Update this type once we have ethereum libs
+    comparator: string,
+    chainId: number = 1,
+): Condition {
+    const conditionConfig = {
+        contractAddress: oracleAddress,
+        method: "latestAnswer",
+        parameters: [],
+        functionAbi: {
+            inputs: [],
+            name: "latestAnswer",
+            outputs: [
+                {
+                    internalType: "int256",
+                    name: "",
+                    type: "int256"
+                }
+            ],
+            stateMutability: "view",
+            type: "function"
+        },
+        chain: chainId,
+        retrurnValueTest: {
+            comparator: comparator,
+            value: triggerPrice
+        }
+    };
+    return new Conditions.Condition(conditionConfig);
+}
+
+
+export async function encrypt(): Promise<void> {
+    const cohort = await Cohort.create(config);
     console.log("Hello from the threshold privacy module!");
 }
