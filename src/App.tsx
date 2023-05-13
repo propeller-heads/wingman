5031888ef273e8800d8f82e4e4db59bf03e4f328
kayibal <alan@datarevenue.com>
2023-05-13 12:22:31 +0100

    Create conditions on button click

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import './App.css';
 import MainForm from './components/stopLoss';
 import { Box } from '@chakra-ui/react';
 import styles from './App.module.css';
+import { createPriceCondition } from './privacy/threshold';
 
 interface MainFormProps {
     onSubmit: (data: any) => void;
@@ -28,15 +29,22 @@ const App: React.FC = () => {
     const handleFormSubmit: MainFormProps['onSubmit'] = (data) => {
         // Handle form submission here
         console.log(data);
+        console.log(nucypher);
+        const condition = createPriceCondition(nucypher, "0x0715A7794a1dc8e42615F059dD6e406A6594651A", 179922000000, "<=", 80001);
+        console.log(condition);
     };
 
     return (
-        <div className={`App ${styles.backgroundImage}`}>
-            <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center">
-                {/* <h1 className={styles.appName}>wingman</h1> */}
-                <MainForm onSubmit={handleFormSubmit} />
-            </Box>
-        </div>
+        nucypher ? (
+            <div className={`App ${styles.backgroundImage}`}>
+                <Box minHeight="100vh" display="flex" alignItems="center" justifyContent="center">
+                    {/* <h1 className={styles.appName}>wingman</h1> */}
+                    <MainForm onSubmit={handleFormSubmit} />
+                </Box>
+            </div>
+        ) : (
+            <div>Loading nucypher, please wait...</div>
+        )
     );
 }
 
--- src/privacy/threshold.test.ts
+++
@@ -1,8 +0,0 @@
-import { createPriceCondition } from './threshold';
-
-describe("test conditions", () => {
-    test("price below validates", async () => {
-        const stopCondition = createPriceCondition("0x0715A7794a1dc8e42615F059dD6e406A6594651A", 179922000000, "<=", 80001);
-        stopCondition.validate();
-    });
-});
